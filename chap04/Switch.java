97ef6110369ab8dee9dd3acb3d5567e6fa3c3b0f
shusuke-iruma-tea <shusuke.pana@gmail.com>
2025-03-08 09:15:10 +0900

    chap04の学習開始

---
+++ chap04/Switch.java
@@ -0,0 +1,23 @@
+package chap04;
+
+public class Switch {
+  public static void main(String[] args) {
+    var rank = "甲";
+
+    switch (rank) {
+      case "甲":
+        System.out.println("大変よいです。");
+        break;
+      case "乙":
+        System.out.println("よいです。");
+        break;
+      case "丙":
+        System.out.println("がんばりましょう。");
+        break;
+
+      default:
+        System.out.println("？？？");
+        break;
+    }
+  }
+}

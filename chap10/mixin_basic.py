e550cec5406e1807c3377291e81fa55922d7d9c4
shusuke-iruma-tea <shusuke.pana@gmail.com>
2025-03-22 11:23:17 +0900

    リスト10.33まで学習完了

---
+++ chap10/mixin_basic.py
@@ -0,0 +1,16 @@
+class LogMixin:
+    def show_attr(self):
+        for key, value in self.__dict__.items():
+            print(f'{key}: {value}')
+
+
+# ミックスインを組み込む
+class Person(LogMixin):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+if __name__ == '__main__':
+    p = Person('鈴木修', 50)
+    p.show_attr()
